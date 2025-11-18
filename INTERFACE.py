4b1896ee6ba70be0063601353350f7b9928e2d2c
GRUPO14D <tecnologia1@grupo14d.com.br>
2025-11-18 09:34:52 -0300

    Subindo atualizações para a realese V 1.0.0

--- INTERFACE.py
+++ INTERFACE.py
@@ -7,6 +7,9 @@ project_root = Path(__file__).parent
 sys.path.insert(0, str(project_root))
 
 from app.ui.app import main
+from app.utils.updater import auto_update
+
+auto_update()
 
 if __name__ == "__main__":
     main()
